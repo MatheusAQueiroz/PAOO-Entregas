b9c31cbfc9b0b899298c2da2d3df0dfe755ec676
MatheusAlves <m-alves7@hotmail.com>
2020-09-05 16:59:52 -0300

    Exercício1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TabelaCursosComponent } from './tabela-cursos/tabela-cursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TabelaCursosComponent
   ],
   imports: [
     BrowserModule
